056884ddeb3d5cd536cb81c8acc5780cb9b785ce
vinluo46 <vinluo46@gmail.com>
2026-01-18 13:01:13 +0800

    Fix base paths for Pages

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,14 @@ import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
 	const posts = (await getCollection('blog')).filter((post) => !post.data.draft);
+	const site = new URL(import.meta.env.BASE_URL, context.site);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
-		site: context.site,
+		site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.id}/`,
+			link: `blog/${post.id}/`,
 		})),
 	});
 }
